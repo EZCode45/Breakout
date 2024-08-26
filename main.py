f6781b2a20f69c62a98e3a777db4e647d589b61e
EZCode45 <eishan.hinduja@gmail.com>
2024-08-26 23:43:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+#-------------------------
+WIDTH = 893
+HEIGHT = 1000
+screen_size = (WIDTH, HEIGHT)
+SCREEN = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('breakout')
+clock = pygame.time.Clock()
+FPS = 120
+#-------------------------
+#color constants
+WHITE = ('#ffffff')
+GREY = ('#a8a8a8')
+BLACK = ('#000000')
+BLUE = ('#000a73')
+RED = ('#820000')
+ORANGE = ('#ff3c00')
+GREEN = ('#00ba09')
+YELLOW = ('#fffb00')
+#-------------------------
+
+while True:
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+    pygame.display.update()
+    clock.tick(FPS)
