3e32667a58cec70fee9a1db3504f17a52502662a
EZCode45 <eishan.hinduja@gamil.com>
2024-09-07 13:53:17 -0400

    qq

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Brick(pygame.sprite.Sprite):
     
 class Paddle(pygame.sprite.Sprite):
   def __init__(self,color, width, height):
-    super().__init()
+    super().__init__()
     self.image = pygame.Surface([width, height])
     pygame.draw.rect(self.image, color, [0, 0, width, height])
     self.rect = self.image.get_rect()
